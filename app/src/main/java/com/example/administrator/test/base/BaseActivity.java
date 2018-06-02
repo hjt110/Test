2def30294b541f58ba57856c5bdd5d7cd9e3cbc0
hjt110 <412700845@qq.com>
2018-06-02 16:25:54 +0800

    test

---
+++ app/src/main/java/com/example/administrator/test/base/BaseActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.test.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private Unbinder mBind;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        mBind = ButterKnife.bind(this);
+        init();
+    }
+
+
+    public abstract int getLayoutResId();
+
+    public abstract void init();
+    
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBind.unbind();
+    }
+}
